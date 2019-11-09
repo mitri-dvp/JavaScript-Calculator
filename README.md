ac9445f435c8a5de8c5c89e8111f75f170cf6751
Jorge Mitri <mitri.dvp@gmail.com>
2019-11-09 20:10:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## JavaScript Calculator  
+This is my third Utility based Web App, a **Calculator** that runs on your browser! Took the design from the MI calculator from my phone, and made all of the **basic** functionality with a little help (Read JS file for more info).  
+### Features  
+  - Basic arithmetic operations:  
+    - Adition.  
+    - Subtraction.  
+    - Multiplication.  
+    - Division.  
+  - Supports **Chain** operations.   
+  - Clear button.  
+   
+This was made with only pure **vanilla JavaScript**.  
+   
+If you are interested in working together, feel free to contact me! <mitri.dvp@gmail.com>  
---
+++ script.js
@@ -0,0 +1,133 @@
+// === IMPORTANT === //
+// My mistake was to treat the entire Output as a single string, instead of making Two strings for each operation, somehow I made that mistake and turn this simple Calculator into a whole mess XD
+// Thanks to Web Dev SiMplified video https://www.youtube.com/watch?v=j59qQ7YWLxw
+// I was able to make this, altough, it's almost the exact same code, I tried my best to simplify it, but it was alredy pretty clean.
+
+// Calculator
+class Calculator {
+  constructor(prev, current) {
+    this.prev = prev;
+    this.current = current;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = '';
+    this.prevOperand = '';
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand + number;
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return;
+    if (this.prevOperand !== '') this.compute();
+    this.operation = operation;
+    this.prevOperand = this.currentOperand;
+    this.currentOperand = '';
+  }
+
+  compute() {
+    let result;
+    const prev = parseFloat(this.prevOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case 'x':
+        result = prev * current;
+        break;
+      case '÷':
+        result = prev / current;
+        break;
+      default:
+        return '';
+    }
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.prevOperand = '';
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    }
+    return integerDisplay;
+  }
+
+  updateDisplay() {
+    this.current.innerText = this.getDisplayNumber(this.currentOperand);
+    if (this.operation != null) {
+      this.prev.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
+    } else {
+      this.prev.innerText = this.prevOperand;
+    }
+  }
+}
+
+// Elements
+const numbers = document.querySelectorAll('.nbr');
+const operations = document.querySelectorAll('.oprd');
+const eq = document.querySelector('.eq');
+const del = document.querySelector('.del');
+const clr = document.querySelector('.clr');
+const prev = document.querySelector('.prev');
+const current = document.querySelector('.current');
+
+// Init
+const calculator = new Calculator(prev, current);
+
+// Append number
+numbers.forEach(btn =>
+  btn.addEventListener('click', () => {
+    calculator.appendNumber(btn.textContent);
+    calculator.updateDisplay();
+  })
+);
+
+// Operations
+operations.forEach(btn =>
+  btn.addEventListener('click', () => {
+    calculator.chooseOperation(btn.textContent);
+    calculator.updateDisplay();
+  })
+);
+
+// Equals
+eq.addEventListener('click', () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+// Clear
+clr.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+// Delete
+del.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
